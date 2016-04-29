ae7820ed109f9e9fea4fcae2b9c09865bbcb210d
mk <mkao006@gmail.com>
2016-04-29 13:01:34 +0200

    corrected the flag implementation

--- R/imputeSingleObservation.R
+++ R/imputeSingleObservation.R
@@ -1,13 +1,36 @@
+##' Make Cross-Validation Groups
+##' 
+##' Creates a vector of cross-validation groups to be used for leave-one-out
+##' cross-validation in later models.
+##' 
+##' @param data A data.table object containing the data.
+##' @param imputationParameters A list of the parameters for the imputation
+##' algorithms.  See defaultImputationParameters() for a starting point.
+##' 
+##' @return A new data table with time series containing sinlge
+##'     observation imputed.
+##' 
+##' @export
+##' 
+
 imputeSingleObservation = function(data, imputationParameters){
     dataCopy = copy(data)
-    param = imputationParameters$
-    dataCopy[, `:=`(c("obsCount"),
-                    sum(is.na(.SD[[param$imputationValueColumn]]))),
+    param = imputationParameters
+    dataCopy[, `:=`("obsCount",
+                    sum(!is.na(.SD[[param$imputationValueColumn]]))),
              by = c(param$byKey)]
 
     dataCopy[obsCount == 1,
-             `:=`(.SD[[param$imputationValueColumn]],
-                  na.omit(.SD[[param$imputationValueColumn]])),
+             `:=`(c(param$imputationValueColumn,
+                  param$imputationFlagColumn,
+                  param$imputationMethodColumn),
+                  list(na.omit(.SD[[param$imputationValueColumn]]),
+                       replace(.SD[[param$imputationFlagColumn]],
+                               which(.SD[[param$imputationFlagColumn]] ==
+                                     param$missingFlag), param$imputationFlag),
+                       replace(.SD[[param$imputationMethodColumn]],
+                               which(.SD[[param$imputationFlagColumn]] ==
+                                     param$missingFlag), param$newMethodFlag))),
              by = c(param$byKey)]
     dataCopy[, `:=`(obsCount, NULL)]
     dataCopy
