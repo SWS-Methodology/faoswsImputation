ef15388a667a23d924ce4be5b2b357077724380e
mk <mkao006@gmail.com>
2016-05-04 12:52:56 +0200

    assign zero for negative imputation related to #issue 13

--- R/defaultExp.R
+++ R/defaultExp.R
@@ -15,6 +15,11 @@ defaultExp = function(x){
     if(all(is.na(x)))
         return(as.numeric(rep(NA_real_, yearCount)))
     expFit = exp(predict(lm(formula = log(x + 1) ~ time),
-        newdata = data.frame(time = time))) - 1
+                         newdata = data.frame(time = time))) - 1
+    ## HACK (Michael): We assign zero to any value that is negative, as it is
+    ##                 bounded. This is because the fit gave a value smaller
+    ##                 than 1 as the model is uncontraint. A better solution is
+    ##                 to have the model bounded.
+    expFit[expFit < 0] = 0
     expFit
 }
